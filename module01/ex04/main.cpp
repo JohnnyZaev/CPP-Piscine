04c2395a66b3000fada7225a250f5472030a7bfd
gvarys <johnnyzaev@icloud.com>
2022-04-02 13:25:55 +0300

    ex04 done && blank main for ex05

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
-#include <cstring>
 #include <fstream>
+#include <sstream>
+
+void RemoveWordFromLine(std::string &line, const std::string &word1, const std::string &word2)
+{
+	size_t n = line.find(word1);
+	while (n != std::string::npos)
+	{
+		line.erase(n, word1.length());
+		line.insert(n, word2);
+		n = line.find(word1);
+	}
+}
 
 int replaceStringInFile(std::string s1, std::string s2, std::string infile)
 {
     std::string outfile;
     std::ifstream fileFrom;
     std::ofstream fileTo;
+	std::stringstream buffer;
 
     outfile = infile + ".replace";
-    if (fileFrom.open(infile))
-        return (1);
-
+    fileFrom.open(infile);
+	if (!fileFrom.is_open())
+		return (1);
+	fileTo.open(outfile);
+	if (!fileTo.is_open())
+		return (1);
+	fileTo.clear();
+	buffer << fileFrom.rdbuf();
+	outfile = buffer.str();
+	RemoveWordFromLine(outfile, s1, s2);
+	fileTo << outfile;
     return (0);
 }
 
@@ -32,6 +52,7 @@ int main(int argc, char **argv)
     if (replaceStringInFile(s1, s2, infile))
     {
         std::cerr << "Files error" << std::endl;
+		return (1);
     }
     std::cout << "Replacement was successful" << std::endl;
     return (0);
