b65c7aff269e72d6c5f8a2cde50f0f3d99247b35
gvarys <johnnyzaev@icloud.com>
2022-05-14 18:37:42 +0300

    ex03 add prototypes

---
+++ module02/ex02/Fixed.hpp
@@ -0,0 +1,51 @@
+#ifndef CPP_PISCINE_FIXED_HPP
+#define CPP_PISCINE_FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+private:
+	int _number;
+	static const int _bits = 8;
+public:
+	Fixed();
+	Fixed(const int number);
+	Fixed(const float number);
+	Fixed(const Fixed &b);
+	~Fixed();
+
+	Fixed& operator= (const Fixed& fixed);
+
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+
+	int toInt( void ) const;
+	float toFloat( void ) const;
+
+	Fixed	&operator++ ( void );
+	Fixed	&operator-- ( void );
+	Fixed	operator++ ( int );
+	Fixed	operator-- ( int );
+
+	bool	operator< ( const Fixed &other ) const;
+	bool	operator> ( const Fixed &other ) const;
+	bool	operator== ( const Fixed &other ) const;
+	bool	operator!= ( const Fixed &other ) const;
+	bool	operator<= ( const Fixed &other ) const;
+	bool	operator>= ( const Fixed &other ) const;
+
+	Fixed	operator+ ( const Fixed &other ) const;
+	Fixed	operator- ( const Fixed &other ) const;
+	Fixed	operator* ( const Fixed &other ) const;
+	Fixed	operator/ ( const Fixed &other ) const;
+
+	static Fixed		&min( Fixed &a, Fixed &b );
+	static const Fixed	&min( const Fixed &a, const Fixed &b );
+	static Fixed		&max( Fixed &a, Fixed &b );
+	static const Fixed	&max( const Fixed &a, const Fixed &b );
+};
+
+std::ostream & operator<< (std::ostream &out, const Fixed &c);
+
+#endif //CPP_PISCINE_FIXED_HPP
