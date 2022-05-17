1069dc3e033193e5ebb6e1d0ffcb2cb84b2a54ba
gvarys <johnnyzaev@icloud.com>
2022-05-17 13:37:38 +0300

    ex02 fixed

--- module02/ex02/main.cpp
+++ module02/ex02/main.cpp
@@ -11,15 +11,16 @@ int main( void ) {
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << "mypart" << std::endl;
 	std::cout << (a < b) << std::endl;
 	std::cout << (a > b) << std::endl;
 	std::cout << (a != b) << std::endl;
 	std::cout << (a <= b) << std::endl;
 	std::cout << (a >= b) << std::endl;
 	std::cout << (a == b) << std::endl;
-	Fixed const c( Fixed( 5.05f ) + Fixed( 2 ) );
-	Fixed const d( Fixed( 5.05f ) - Fixed( 2 ) );
-	Fixed const e( Fixed( 5.05f ) / Fixed( 2 ) );
+	Fixed const c( Fixed( 5.f ) + Fixed( 2 ) );
+	Fixed const d( Fixed( 5.f ) - Fixed( 2 ) );
+	Fixed const e( Fixed( 5.f ) / Fixed( 2 ) );
 	std::cout << c << " " << d << " " << e << std::endl;
 	return 0;
 }
