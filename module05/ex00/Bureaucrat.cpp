271238a4f80b9354c761624267f80def1c541538
gvarys <johnnyzaev@icloud.com>
2022-06-02 22:01:20 +0300

    module 5 ex00 working fine

--- module05/ex00/Bureaucrat.cpp
+++ module05/ex00/Bureaucrat.cpp
@@ -13,15 +13,11 @@ Bureaucrat::~Bureaucrat() {
 }
 
 Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name), _grade(grade) {
-	try {
-		if (_grade < 1)
-			throw GradeTooHighException();
-		else if (_grade > 150)
-			throw GradeTooLowException();
-	}
-	catch (std::exception &e) {
-		std::cout << "Problem exist between keyboard and chair" << std::endl;
-	}
+	std::cout << "Bureaucrat is created" << std::endl;
+	if (_grade < 1)
+		throw GradeTooHighException();
+	else if (_grade > 150)
+		throw GradeTooLowException();
 }
 
 Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other) {
@@ -30,6 +26,7 @@ Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other) {
 }
 
 Bureaucrat::Bureaucrat(const Bureaucrat &other) : _name(other._name) {
+	std::cout << "Bureaucrat is created" << std::endl;
 	*this = other;
 }
 
@@ -50,27 +47,18 @@ const char *Bureaucrat::GradeTooHighException::what() const throw() {
 }
 
 void Bureaucrat::decrementGrade() {
-	try{
-		if (++_grade > 150)
-			throw GradeTooLowException();
-	}
-	catch (std::exception &e) {
-		std::cout << "Problem exist between keyboard and chair" << std::endl;
-	}
+	if (++_grade > 150)
+		throw GradeTooLowException();
+
 }
 
 void Bureaucrat::incrementGrade() {
-	try{
-		if (--_grade > 1)
-			throw GradeTooHighException();
-	}
-	catch (std::exception &e) {
-		std::cout << "Problem exist between keyboard and chair" << std::endl;
-	}
+	if (--_grade < 1)
+		throw GradeTooHighException();
 }
 
 std::ostream &operator<<(std::ostream &out, Bureaucrat const &bureaucrat)
 {
-	out << "Bureaucrat " << bureaucrat.getName() << " (Grade " << bureaucrat.getGrade() << ")";
+	out  << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade();
 	return (out);
 }
