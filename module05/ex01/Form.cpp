0e031b4a5e0f97c834f47b68a50efa08592a445c
gvarys <johnnyzaev@icloud.com>
2022-06-04 18:39:09 +0300

    module 5 ex01 form prerequisite

---
+++ module05/ex01/Form.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Johnny Zaev on 04.06.2022.
+//
+
+#include "Form.hpp"
---
+++ module05/ex01/Form.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Johnny Zaev on 04.06.2022.
+//
+
+#ifndef CPP_PISCINE_FORM_HPP
+#define CPP_PISCINE_FORM_HPP
+
+
+class Form {
+
+};
+
+
+#endif //CPP_PISCINE_FORM_HPP
