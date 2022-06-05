d0a3f3200e862855fc2179be17051b5e4a5cf900
gvarys <johnnyzaev@icloud.com>
2022-06-05 17:30:40 +0300

    module 5 ex01 form header files

--- module05/ex01/Form.hpp
+++ module05/ex01/Form.hpp
@@ -5,8 +5,18 @@
 #ifndef CPP_PISCINE_FORM_HPP
 #define CPP_PISCINE_FORM_HPP
 
+#include <iostream>
 
 class Form {
+private:
+	const std::string _name;
+	bool isSigned;
+	const int gradeToSign;
+	const int gradeToExecute
+public:
+	Form();
+	Form(const Form &other);
+	~Form();
 
 };
 
