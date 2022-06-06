6890dc5cdd9507c1ba161dab5e9b8fab8588fd26
gvarys <johnnyzaev@icloud.com>
2022-06-06 13:29:04 +0300

    module 5 ex01 form constructors

--- module05/ex01/Form.cpp
+++ module05/ex01/Form.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "Form.hpp"
+
+Form::Form() : _name("Form"), _isSigned(false), _gradeToSign(150), _gradeToExecute(150) {}
+
+Form::~Form() {}
+
+Form::Form(std::string name, int gradeToSign, int gradeToExecute) : _name(name), _isSigned(false), _gradeToSign(gradeToSign), _gradeToExecute(gradeToExecute) {
+	if (gradeToSign > 150 || gradeToExecute > 150)
+		throw GradeTooLowException();
+	if (gradeToSign < 1 || gradeToExecute < 1)
+		throw GradeTooHighException();
+}
+
+Form::Form(const Form &other) : _name(other._name), _isSigned(other._isSigned), _gradeToSign(other._gradeToSign), _gradeToExecute(other._gradeToExecute) {
+
+}
+
+Form &Form::operator=(const Form &other) {
+	*this(Form(other));
+	return *this;
+}
--- module05/ex01/Form.hpp
+++ module05/ex01/Form.hpp
@@ -10,15 +10,31 @@
 class Form {
 private:
 	const std::string _name;
-	bool isSigned;
-	const int gradeToSign;
-	const int gradeToExecute
+	bool _isSigned;
+	const int _gradeToSign;
+	const int _gradeToExecute;
 public:
 	Form();
 	Form(const Form &other);
+	Form(std::string name, int gradeToSign, int gradeToExecute);
 	~Form();
 
+	std::string getName();
+	bool isSigned();
+	int getGradeToSign();
+	int getGradeToExecute();
+
+	Form &operator=(const Form &other);
+
+	class GradeTooHighException: public std::exception {
+		virtual const char* what() const throw();
+	};
+	class GradeTooLowException: public std::exception {
+		virtual const char* what() const throw();
+	};
 };
+
+std::ostream &operator<<(std::ostream &out, Form const &form);
 
 
 #endif //CPP_PISCINE_FORM_HPP
