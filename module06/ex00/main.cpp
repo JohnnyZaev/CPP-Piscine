d6daccf2db8dfd92939cac09c9534a7310f9043f
gvarys <johnnyzaev@gmail.com>
2022-07-02 17:09:27 +0300

    module06 ex00 starter get str and argc check

--- module06/ex00/main.cpp
+++ module06/ex00/main.cpp
@@ -2,7 +2,15 @@
 // Created by Johnny Zaev on 30.06.2022.
 //
 
-int main( void )
+#include <iostream>
+
+int main(int argc, char** argv)
 {
+	if (argc != 2)
+	{
+		std::cout << "Not enough arguments" << std::endl;
+		return (EXIT_FAILURE);
+	}
+	std::string str = static_cast<std::string>(argv[1]);
 	return 0;
 }
