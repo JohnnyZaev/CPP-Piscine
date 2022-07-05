ccda94037a0b89c6eb97060eee916daab7ed78b8
gvarys <johnnyzaev@gmail.com>
2022-07-05 21:28:37 +0300

    module06 ex00 start

--- module06/ex01/main.cpp
+++ module06/ex01/main.cpp
@@ -4,6 +4,11 @@
 
 #include <iostream>
 
+void castsmth(std::string str)
+{
+	std::cout << "Do smth" << std::endl;
+}
+
 int main(int argc, char** argv)
 {
 	if (argc != 2)
@@ -12,5 +17,6 @@ int main(int argc, char** argv)
 		return (EXIT_FAILURE);
 	}
 	std::string str = static_cast<std::string>(argv[1]);
+	castsmth(str);
 	return 0;
 }
