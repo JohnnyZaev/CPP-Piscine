0df56e4e39040a5739f3db3294c8276c8acd3d0c
gvarys <johnnyzaev@gmail.com>
2022-07-08 16:10:42 +0300

    module06 ex01 done!

--- module06/ex01/main.cpp
+++ module06/ex01/main.cpp
@@ -23,6 +23,22 @@ Data* deserialize(uintptr_t raw)
 
 int main(void)
 {
-
+	Data		*ptr = new Data;
+	Data		*new_ptr;
+	uintptr_t	raw;
+	ptr->str = "KEKW";
+	ptr->i = 42;
+	ptr->f = 42.5f;
+	std::cout << "ptr address = " << ptr << std::endl;
+	raw = serialize(ptr);
+	std::cout << "raw address = " << raw << std::endl;
+	new_ptr = deserialize(raw);
+	std::cout << "new_ptr address = " << raw << std::endl;
+//	new_ptr->i = 25; // if we try to assign another value to new_ptr, value in ptr will be also changed;
+//	std::cout << "ptr->str = " << ptr->i << std::endl;
+	std::cout << "new_ptr->str = " << new_ptr->str << std::endl;
+	std::cout << "new_ptr->i = " << new_ptr->i << std::endl;
+	std::cout << "new_ptr->f = " << new_ptr->f << std::endl;
+	delete ptr;
 	return EXIT_SUCCESS;
 }
