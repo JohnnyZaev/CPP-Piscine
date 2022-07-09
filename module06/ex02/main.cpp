59b75f646728be5f3103bec8ed1e17dbc705612c
gvarys <johnnyzaev@gmail.com>
2022-07-09 13:26:58 +0300

    module06 ex02 looks like done! need more tests

--- module06/ex02/main.cpp
+++ module06/ex02/main.cpp
@@ -3,11 +3,13 @@
 //
 
 #include <iostream>
+#include <stdlib.h>
 
 class Base
 {
 public:
-	virtual ~Base();
+	virtual ~Base() {};
+	Base() {};
 };
 
 class A : public Base
@@ -22,6 +24,59 @@ class C : public Base
 {
 };
 
+Base * generate(void)
+{
+	Base* base;
+	int n = rand() % 3;
+
+	if (n == 0)
+		base = new A();
+	if (n == 1)
+		base = new B();
+	if (n == 2)
+		base = new C();
+	return base;
+}
+
+void identify(Base* p)
+{
+	if (p == NULL)
+	{
+		std::cout << "NULL" << std::endl;
+		return;
+	}
+	A* baseA;
+	B* baseB;
+	C* baseC;
+
+	baseA = dynamic_cast<A *>(p);
+	baseB = dynamic_cast<B *>(p);
+	baseC = dynamic_cast<C *>(p);
+	if (baseA)
+		std::cout << "A" << std::endl;
+	if (baseB)
+		std::cout << "B" << std::endl;
+	if (baseC)
+		std::cout << "C" << std::endl;
+}
+
+void identify(Base& p)
+{
+	A* baseA;
+	B* baseB;
+	C* baseC;
+
+	baseA = dynamic_cast<A *>(&p);
+	baseB = dynamic_cast<B *>(&p);
+	baseC = dynamic_cast<C *>(&p);
+	if (baseA)
+		std::cout << "A" << std::endl;
+	if (baseB)
+		std::cout << "B" << std::endl;
+	if (baseC)
+		std::cout << "C" << std::endl;
+}
+
 int main(int argc, char** argv)
 {
 	if (argc != 2)
@@ -29,6 +84,21 @@ int main(int argc, char** argv)
 		std::cout << "Not enough arguments" << std::endl;
 		return (EXIT_FAILURE);
 	}
-	std::string str = static_cast<std::string>(argv[1]);
-	return 0;
+	size_t times;
+	try { times = std::stoul(argv[1]); }
+	catch (std::invalid_argument&)
+	{
+		std::cout << "Pass an integer" << std::endl;
+		return EXIT_FAILURE;
+	}
+	Base *base;
+	while (times)
+	{
+		base = generate();
+		identify(base);
+		identify(*base);
+		delete base;
+		times--;
+	}
+	return EXIT_SUCCESS;
 }
