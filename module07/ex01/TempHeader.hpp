714243682e4b51f8f50334a1e6e4cb4632cbbdb9
gvarys <johnnyzaev@gmail.com>
2022-07-14 15:12:52 +0300

    module07 ex01 done!

--- module07/ex01/TempHeader.hpp
+++ module07/ex01/TempHeader.hpp
@@ -8,7 +8,7 @@
 #include <iostream>
 
 template<typename T>
-void iter(T *array, std::size_t len, void (*func)(const T&))
+void iter(T *array, std::size_t len, void (*func)(T&))
 {
 	for (std::size_t i = 0; i < len; i++)
 		func(array[i]);
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -5,24 +5,23 @@
 #include <iostream>
 #include "TempHeader.hpp"
 
+void minmin(int &a)
+{
+	a--;
+}
+
+template<typename T>
+void printa(T &a)
+{
+	std::cout << a << std::endl;
+}
+
 int main( void )
 {
-	int a = 5;
-	int b(a);
-	int c(10);
-	std::cout << a << b << c << std::endl;
-	swap(a, c);
-	std::cout << a << b << c << std::endl;
-	std::cout << ::min(a, c) << std::endl;
-	std::cout << ::max(a, b) << std::endl;
-	std::cout << ::min(c, b) << std::endl;
-	std::cout << ::max(b, c) << std::endl;
-	std::string str1 = "abc";
-	std::string str2 = "cba";
-	std::cout << str1 << str2 << std::endl;
-	swap(str1, str2);
-	std::cout << str1 << str2 << std::endl;
-	std::cout << ::min(str1, str2) << std::endl;
-	std::cout << ::max(str1, str2) << std::endl;
+	int a[5] = {10, 9 ,8 ,7 ,6};
+	::iter(a, static_cast<std::size_t>(sizeof(a)/sizeof(a[0])), &minmin);
+	::iter(a, static_cast<std::size_t>(sizeof(a)/sizeof(a[0])), &printa);
+	std::string b[] = {"HATE", "THIS", "DAY"};
+	::iter(b, static_cast<std::size_t>(sizeof(b)/sizeof(b[0])), &printa);
 	return EXIT_SUCCESS;
 }
