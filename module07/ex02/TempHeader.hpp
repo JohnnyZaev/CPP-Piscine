d126f3009968b722ab8c11e20d04700e8659c088
gvarys <johnnyzaev@gmail.com>
2022-07-14 15:25:47 +0300

    module07 ex02 starter files

---
+++ module07/ex02/TempHeader.hpp
@@ -0,0 +1,18 @@
+//
+// Created by Johnny Zaev on 13.07.2022.
+//
+
+#ifndef CPP_PISCINE_TEMPHEADER_HPP
+#define CPP_PISCINE_TEMPHEADER_HPP
+
+#include <iostream>
+
+template<typename T>
+void iter(T *array, std::size_t len, void (*func)(T&))
+{
+	for (std::size_t i = 0; i < len; i++)
+		func(array[i]);
+}
+
+
+#endif //CPP_PISCINE_TEMPHEADER_HPP
