9ce6848a4387019025389989bd51bb6ff5ceeca2
gvarys <johnnyzaev@gmail.com>
2022-07-15 11:56:54 +0300

    module08 ex01 starter files

---
+++ module08/ex00/main.cpp
@@ -0,0 +1,44 @@
+//
+// Created by Johnny Zaev on 30.06.2022.
+//
+
+#include "Array.hpp"
+
+int main( void )
+{
+	Array<int>			intArray;
+	Array<std::string>	stringArray(7);
+	Array<std::string>	otherStringArray;
+	stringArray[0] = "Hi";
+	stringArray[1] = "Hello";
+	stringArray[2] = "How are you?";
+	stringArray[3] = "Fine and you?";
+	stringArray[4] = "Doing great thanks!";
+	stringArray[5] = "You're very welcome";
+	std::cout << "intArray.size() = " << intArray.Size() << std::endl;
+	std::cout << "stringArray.size() = " << stringArray.Size() << std::endl;
+	for (unsigned int i = 0; i < stringArray.Size(); i++)
+		std::cout << stringArray[i] << std::endl;
+	otherStringArray = stringArray;
+	otherStringArray[3] = "I'm good, how about you?";
+	for (unsigned int i = 0; i < otherStringArray.Size(); i++)
+		std::cout << otherStringArray[i] << std::endl;
+	try
+	{
+		std::cout << stringArray[59] << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	try
+	{
+		std::cout << stringArray[-3] << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	return EXIT_SUCCESS;
+}
