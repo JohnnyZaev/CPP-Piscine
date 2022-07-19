c37112b5ebfa3a6af335132a55b1fed7fc809f4c
gvarys <johnnyzaev@gmail.com>
2022-07-19 14:54:48 +0300

    module08 ex00 easyfind almost done! need more tests

--- module08/ex00/easyfind.hpp
+++ module08/ex00/easyfind.hpp
@@ -5,4 +5,14 @@
 #ifndef CPP_PISCINE_EASYFIND_HPP
 #define CPP_PISCINE_EASYFIND_HPP
 
+#include <algorithm>
+#include <iostream>
+
+template<typename T>
+typename T::iterator easyfind(T &cont, int value)
+{
+	return find(cont.begin(), cont.end(), value);
+}
+
+
 #endif //CPP_PISCINE_EASYFIND_HPP
--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -3,9 +3,19 @@
 //
 
 #include <iostream>
+#include <vector>
 #include <algorithm>
+#include "easyfind.hpp"
 
 int main( void )
 {
+	std::vector<int> vect;
+	std::vector<int>::iterator it;
+	for (int i = 1; i < 59; i += 2)
+		vect.push_back(i);
+	it = easyfind(vect, 43);
+	std::cout << *it << std::endl;
+	it = easyfind(vect, 42);
+	std::cout << *it << std::endl;
 	return EXIT_SUCCESS;
 }
