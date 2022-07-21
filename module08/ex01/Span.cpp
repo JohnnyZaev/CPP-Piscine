30981994874fd49f3ac90acd2ef1aec35127bd09
gvarys <johnnyzaev@gmail.com>
2022-07-21 15:29:09 +0300

    module08 ex01 new starter files

---
+++ module08/ex01/Span.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Johnny Zaev on 21.07.2022.
+//
+
+#include "Span.hpp"
---
+++ module08/ex01/Span.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Johnny Zaev on 21.07.2022.
+//
+
+#ifndef CPP_PISCINE_SPAN_HPP
+#define CPP_PISCINE_SPAN_HPP
+
+#include <iostream>
+
+class Span {
+
+};
+
+
+#endif //CPP_PISCINE_SPAN_HPP
