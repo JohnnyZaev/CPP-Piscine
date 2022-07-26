6a6eb42baf6f6d5d175581c17f0385911ffeddbc
gvarys <johnnyzaev@gmail.com>
2022-07-26 16:25:58 +0300

    module08 ex02 MutantStack works fine

--- module08/ex02/MutantStack.hpp
+++ module08/ex02/MutantStack.hpp
@@ -5,4 +5,19 @@
 #ifndef CPP_PISCINE_MUTANTSTACK_HPP
 #define CPP_PISCINE_MUTANTSTACK_HPP
 
+# include <stack>
+# include <iostream>
+
+template<typename T>
+class MutantStack: public std::stack<T>
+{
+public:
+	typedef typename std::stack<T>::container_type::iterator iterator;
+
+	iterator begin() { return this->c.begin(); }
+	iterator end() { return this->c.end(); }
+
+	iterator begin() const { return this->c.begin(); }
+	iterator end() const { return this->c.end(); }
+};
 #endif //CPP_PISCINE_MUTANTSTACK_HPP
--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -6,5 +6,27 @@
 
 int main( void )
 {
+	std::stack<int> stack;
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+	std::cout << mstack.size() << std::endl;
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
 	return EXIT_SUCCESS;
 }
